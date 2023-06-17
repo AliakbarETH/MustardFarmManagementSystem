c0bf0de2dc334eb6e8c5b31b6dee2aa2a4074dd1
AliakbarETH <aliakbar.bcss19@iba-suk.edu.pk>
2023-06-17 15:03:50 +0500

    Features required for Crop Management

--- Backend/Backend.java
+++ Backend/Backend.java
@@ -7,4 +7,9 @@ public class Backend {
 /*
  * Features we need to develop
  * Crop Management(crop_types, planting_dates, harvesting_dates, yeild_information)
+ * Features we need:
+     1) Know crop Types
+     2) Planting Dates
+     3) Harvesting Dates
+     4) Yield Information
  */
