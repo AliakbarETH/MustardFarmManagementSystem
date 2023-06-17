9e01f5deb15203209150634f61a55f69e5d24f95
AliakbarETH <aliakbar.bcss19@iba-suk.edu.pk>
2023-06-17 15:18:53 +0500

    Worked on feature docs

--- Backend/Backend.java
+++ Backend/Backend.java
@@ -6,7 +6,6 @@ public class Backend {
 
 /*
  * Features we need to develop
- * Crop Management(crop_types, planting_dates, harvesting_dates, yeild_information)
  * Inventory Management(seeds, fertilizers, pesticides, machinery)
  * Features we need:
      1) Maintain an inventory of resources
