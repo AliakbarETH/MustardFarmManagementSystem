614d970be29bd01c1bfbebc318b5e567a0fac050
AliakbarETH <aliakbar.bcss19@iba-suk.edu.pk>
2023-06-17 15:14:29 +0500

    Worked on Financial Tracking Features

--- Backend/Backend.java
+++ Backend/Backend.java
@@ -7,4 +7,9 @@ public class Backend {
 /*
  * Features we need to develop
  * Crop Management(crop_types, planting_dates, harvesting_dates, yeild_information)
+ * FinancialTracking
+ * Features we need:
+     1) Keep track of income and expenses
+     2) Generate reports and summaries
+     3) Monitor profitability
  */
