a2f5ec28e2aa1f57ded22dffe184666f453bc9bf
AliakbarETH <aliakbar.bcss19@iba-suk.edu.pk>
2023-06-17 15:02:31 +0500

    Inventory Management System Features

--- Backend/Backend.java
+++ Backend/Backend.java
@@ -7,4 +7,9 @@ public class Backend {
 /*
  * Features we need to develop
  * Crop Management(crop_types, planting_dates, harvesting_dates, yeild_information)
+ * Inventory Management(seeds, fertilizers, pesticides, machinery)
+ * Features we need:
+     1) Maintain an inventory of resources
+     2) Track the quantity of each item
+     3) Alert when stock levels are low
  */
